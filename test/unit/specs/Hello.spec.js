e7c6f90ca52a1d7ae28051337536bdc5d9a4e497
Antoniel Cardoso Martins <antonielcmartins@outlook.com>
2017-06-26 17:22:51 -0400

    Creating the first snapshot test

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -9,4 +9,16 @@ describe('Hello.vue', () => {
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toBe('Welcome to Your Vue.js App')
   })
+
+  it('renders a snapshot', () => {
+    const renderer = require('vue-server-renderer').createRenderer();
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: h => h(Hello),
+    });
+
+    renderer.renderToString(vm, (err, str) => {
+      expect(str).toMatchSnapshot();
+    });
+  })
 })
